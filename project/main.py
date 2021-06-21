240d82e35d752eacfaca7131d8dace29d362f1a1
SitD <sitd0813@gmail.com>
2021-06-21 11:38:01 +0900

    EAR None 수정

--- project/main.py
+++ project/main.py
@@ -5,9 +5,8 @@ import cv2
 
 from alarm import ring_alarm
 from ear import calculate_ear
-# from head_pose import calculate_head_pose
+from head_pose import calculate_head_pose
 from preprocess import preprocess
-import head_pose
 
 EAR_DROWSY_FLAG = False
 EAR_DROWSY_RATIO = 0.5
@@ -44,31 +43,32 @@ if __name__ == "__main__":
         # 전처리
         L, gray, rgb, bgr = preprocess(frame)
 
+        # head pose 계산
+        # headPose = calculate_head_pose(rgb, bgr)
+
         # EAR 계산
         ear = calculate_ear(rgb, draw=bgr)
-        # EAR 최솟값과 최댓값 사이의 값을 임계값으로 지정
-        if ear > EAR_MAX:
-            EAR_MAX = ear
-        if ear < EAR_MIN:
-            EAR_MIN = ear
-        EAR_THRESH = EAR_DROWSY_RATIO * EAR_MAX + (1 - EAR_DROWSY_RATIO) * EAR_MIN
+        if ear is not None:
+            # EAR 최솟값과 최댓값 사이의 값을 임계값으로 지정
+            if ear > EAR_MAX:
+                EAR_MAX = ear
+            if ear < EAR_MIN:
+                EAR_MIN = ear
+            EAR_THRESH = EAR_DROWSY_RATIO * EAR_MAX + (1 - EAR_DROWSY_RATIO) * EAR_MIN
 
-        # head pose 계산
-        headPose = head_pose.calculate_head_pose(rgb, bgr)
+            # `ear`이 `EAR_THRESH`보다 `TIME_THRESH` 초 이상 낮으면 졸음으로 판단
+            if ear < EAR_THRESH:
+                if not EAR_DROWSY_FLAG:
+                    START = timeit.default_timer()
+                    EAR_DROWSY_FLAG = True
 
-        # `ear`이 `EAR_THRESH`보다 `TIME_THRESH` 초 이상 낮으면 졸음으로 판단
-        if ear < EAR_THRESH:
-            if not EAR_DROWSY_FLAG:
-                START = timeit.default_timer()
-                EAR_DROWSY_FLAG = True
-
-            NOW = timeit.default_timer()
-            if (NOW - START) > TIME_THRESH:
-                cv2.putText(bgr, 'EAR_Drowsiness!', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3, cv2.LINE_AA)
-                #print('drowsiness...')
-                ring_alarm()
-        else:
-            EAR_DROWSY_FLAG = False
+                NOW = timeit.default_timer()
+                if (NOW - START) > TIME_THRESH:
+                    cv2.putText(bgr, 'EAR_Drowsiness!', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3, cv2.LINE_AA)
+                    #print('drowsiness...')
+                    ring_alarm()
+            else:
+                EAR_DROWSY_FLAG = False
 
         # print(f"EAR = {ear}")
         # print(f"Head pose = {head_pose})
